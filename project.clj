1fcb3ab49493a5041e0ed8bbd84d9e82bbc95d88
Ian McEwen <mian@cyverse.org>
2020-01-24 13:41:30 -0700

    Version 0.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/async-tasks-client "0.0.2"
+(defproject org.cyverse/async-tasks-client "0.0.3-SNAPSHOT"
   :description "Client for the async-tasks service"
   :url "https://github.com/cyverse-de/async-tasks-client"
   :license {:name "BSD"
